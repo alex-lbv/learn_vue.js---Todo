17c2a56f7b78660413e49f756dbfe203d43b75d6
MacBook <aal940722@gmail.com>
2020-09-30 12:34:25 +0300

    feature: adds files Todo

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dolores error fuga harum illum, ipsa nemo
+      odit quaerat repudiandae ut.</p>
+
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Home"
+  }
+</script>
+
+<style scoped>
+
+</style>
